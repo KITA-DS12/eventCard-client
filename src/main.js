ea96d1626cb46899d23c815b628afb3222f40535
KITA-DS12 <kita.ds12@gmail.com>
2023-04-29 13:16:16 +0900

    add ec2

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8888/";
+axios.defaults.baseURL = "http://3.25.70.38:8888/";
 axios.defaults.withCredentials = true;
 
 loadFonts();
